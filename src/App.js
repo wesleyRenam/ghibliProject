17599cc2214205ffe4815b30c129daccf538c570
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2023-01-18 11:26:43 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Route, Switch } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Favorites from './pages/Favorites';
+
+function App() {
+  return (
+    <Switch>
+      <Route exact path="/" component={ Home } />
+      <Route path="/favorites" component={ Favorites } />
+    </Switch>
+  );
+}
+
+export default App;
