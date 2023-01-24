b22c715bf649cfff4351cc78c88e0f7a7d81e651
wesleyRenam <wesleyrenamjob@gmail.com>
2023-01-24 16:02:16 -0300

    pagina de favoritos estilizada

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -6,23 +6,33 @@ import FilmsContext from '../context/FilmsContext';
 export default function Favorites() {
   const { movies, favorites, removeFavorite } = useContext(FilmsContext);
   return (
-    <div>
+    <div className='bg-babyblue h-screen'>
       <Header/>
       <h2> Favoritos </h2>
+      <div className="flex flex-row flex-wrap items-center justify-center bg-babyblue">
       {
         favorites.map((movie) => (
-          <div key={ movie.id } className="repo">
-            <p>{movie.original_title_romanised}</p>
-            <img src={ movie.movie_banner } alt={ movie.original_title_romanised } />
-            <button
-              type="submit"
-              onClick={ () => removeFavorite(movie) }
-            >
-              Desfavoritar
-            </button>
+          <div key={ movie.id } className="" >
+            <div className="rounded-lg shadow-2xl bg-navyblue p-3 pb-4 max-w-2xs m-3
+            sm:max-w-3xs min-[432px]:max-w-4xs min-[390px]:max-w-xs">
+              <img src={ movie.image } alt={ movie.original_title_romanised } className=""/>
+              <div className='flex items-center justify-between mt-3'>
+                <p className='text-xl items-center text-white'>{movie.title}</p>
+                <button
+                  id="submitBtn"
+                  type="submit"
+                  onClick={() => removeFavorite(movie) }
+                  className='p-2 rounded-md'
+                  >
+                 {}
+                 <img className='w-7' alt="favorito" src={!favorites.some((favorite) => favorite.id === movie.id) ? require('../img/starWhite.png') : require('../img/starBlueGhibli.png')}/>
+                </button>
+              </div>
+            </div>
           </div>
         ))
       }
+      </div>
     </div>
   );
 }
