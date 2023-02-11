44903644517d398c17b63c0fbb15c1030674aff2
wesleyRenam <wesleyrenam@gmail.com>
2023-02-11 09:27:56 -0300

    Arrumei search

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -8,10 +8,11 @@ import Loading from './Loading';
 function Movies() {
   const { movies, saveFavorite, favorites, removeFavorite, inputText, isLoading, setInputText } = useContext(FilmsContext);
   const {hideSearch} = useContext(LoginContext);
-  const filterMovies = movies.filter((mov) => mov.title.toLowerCase().includes(inputText ))
+  const regex = /^[a-z\u00E0-\u00FC]+$/i;
+  const filterMovies = movies.filter((mov) => mov.title.toLowerCase().includes(inputText.toLowerCase()))
   return (
     <div className='h-screen bg-babyblue '>
-      <div className= {hideSearch ? 'hidden' : 'hidden max-[500px]:flex justify-center items-center'}>
+      <div className= {hideSearch ? 'hidden' : 'hidden max-[500px]:flex justify-center items-center '}>
         <input type="text" onChange = {(e) => setInputText(e.target.value)} className='mx-1 flex border-solid border-2 border-navyblue rounded-lg p-3 bg-navyblue text-white w-full max-'/>  
         <img alt="starHome"className='w-8 absolute right-6 ' src={require('../img/lupa.png')}/>
       </div>
