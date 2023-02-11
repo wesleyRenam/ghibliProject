908e79a7e8d0f0eb0caac49463df0f86c333cf01
wesleyRenam <wesleyrenam@gmail.com>
2023-02-11 11:24:07 -0300

    color search arrumado

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -12,7 +12,7 @@ function Movies() {
   return (
     <div className={`'h-screen`} style={{backgroundColor: !dark ? '#58B7EE' : '#144552' }}>
       <div className= {hideSearch ? 'hidden' : 'hidden max-[500px]:flex justify-center items-center '}>
-        <input type="text" onChange = {(e) => setInputText(e.target.value)} className='mx-1 flex border-solid border-2 border-navyblue rounded-lg p-3 bg-navyblue text-white w-full max-'/>  
+        <input type="text" onChange = {(e) => setInputText(e.target.value)} className='mx-1 flex border-solid border-2 border-navyblue rounded-lg p-3 text-white w-full max-' style={{backgroundColor: !dark ? '#05445E' : '#0B171E' }}/>  
         <img alt="starHome"className='w-8 absolute right-6 ' src={require('../img/lupa.png')}/>
       </div>
       
