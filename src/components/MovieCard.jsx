04d36c470c3043b59cd3da06f04180a9261167ea
wesleyRenam <wesleyrenam@gmail.com>
2023-02-11 23:48:48 -0300

    .

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -22,7 +22,7 @@ function MovieCard () {
         </div>
         {(!movies.length) ? <Loading /> :
           <div className='bg-babyblue text-white h-screen flex justify-center items-center '>
-              <div className="rounded-lg shadow-2xl bg-navyblue p-4 flex h-[80vh] w-[65vw] flex-col items-center py-5 space-y-2">
+              <div className="rounded-lg shadow-2xl bg-navyblue p-4 flex h-[80vh] w-[65vw] max[600px]:w-[80vw] flex-col items-center py-5 space-y-2">
                 <h1 className="text-2xl">{movie[0].title}</h1> 
                 <img src={movie[0].movie_banner} alt="" className="" width="500rem"/>
                 <h2>Titulo Original: {movie[0].original_title}</h2>
