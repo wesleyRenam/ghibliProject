90af4e58aed43b92b797c504a8e5bf2fa5c219f8
wesleyRenam <wesleyrenam@gmail.com>
2023-03-18 21:27:44 -0300

    Botão voltar detalhes filme

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -11,20 +11,20 @@ function MovieCard () {
     const { dark, setDark } = useContext(LoginContext);
     const movie = movies.filter((m) => m.id === id);
     return (
-      <div className="bg-babyblue h-screen">
-        <div className="flex justify-between pb-0">
-          <Link to="/home">
-            <img src={require('../img/arrowIcon2.png')} alt="arrow" />
-          </Link>
-          <button onClick = {() => setDark(!dark )}>
-            <img alt="darkbtn"className='w-11'src={ dark ? require('../img/sun.png') : require('../img/half-moon.png')}/>
-          </button>
-        </div>
+      <div className="h-screen">
         {(!movies.length) ? <Loading /> :
-          <div className='bg-babyblue text-white h-screen flex justify-center items-center '>
-              <div className="rounded-lg shadow-2xl bg-navyblue p-4 flex h-[80vh] w-[65vw] max-[600px]:w-[80vw] flex-col items-center py-5 space-y-2">
-                <h1 className="text-2xl">{movie[0].title}</h1> 
-                <img src={movie[0].movie_banner} alt="" className="" width="500rem"/>
+          <div className='text-white h-screen flex justify-center items-center' style={{backgroundColor: !dark ? '#58B7EE' : '#144552' }}>
+              <div className="rounded-lg shadow-2xl p-4 flex h-[80vh] w-[65vw] max-[600px]:w-[80vw] flex-col items-center py-5 space-y-2" style={{backgroundColor: !dark ? '#05445E' : '#162E3C' }}>
+                <div className="flex items-center w-[80vw] justify-around">
+                  <Link to="/home">
+                    <img src={require('../img/arrowIcon2.png')} alt="arrow" />
+                  </Link>
+                  <h1 className="text-2xl">{movie[0].title}</h1> 
+                  <button onClick = {() => setDark(!dark )}>
+                    <img alt="darkbtn"className='w-11'src={ dark ? require('../img/sun.png') : require('../img/half-moon.png')}/>
+                  </button>
+                </div>
+                <img src={movie[0].movie_banner} alt="" className="" width="600rem"/>
                 <h2>Titulo Original: {movie[0].original_title}</h2>
                 <h2>Diretor: {movie[0].director}</h2>
                 <p> {movie[0].description} </p>
